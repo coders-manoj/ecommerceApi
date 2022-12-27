c5ec0087ca2292ad000ba1ae3334089d0f43483a
manoj kushwah <manojkushwah1066@gmail.com>
2022-12-28 01:15:38 +0530

    install and integrate crypto-js

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,20 @@
 const router = require("express").Router();
 const user = require("../models/userSchema");
+var CryptoJS = require("crypto-js");
 
 // user register
 router.post("/register", async (req, res) => {
   try {
     const newUser = new user({
       ...req.body,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.CRYPTO_SECRET
+      ).toString(),
     });
     const response = await user.create(newUser);
     res.status(201).json({
-      succss: true,
+      success: true,
       message: "user registered success",
       data: response,
       error: false,
@@ -17,8 +22,8 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     const key = Object.keys(error.keyValue);
     res.status(401).json({
-      succss: false,
-      message: `${key[0]} should be unique`,
+      success: false,
+      message: `this ${key[0]} already exists`,
       data: error.keyValue,
       error: true,
     });
